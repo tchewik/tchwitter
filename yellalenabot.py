afaf02c62f5c8bbd2f81314837bc5099c238889a
tchewik <elenachistov@gmail.com>
2016-04-17 02:37:23 +0300

    исправлена возможность исключения слов

--- yellalenabot.py
+++ yellalenabot.py
@@ -10,6 +10,7 @@ if len(sys.argv) == 6:
 	escapeword = str(sys.argv[5])
 else:
 	escapeword = "ESCAPEWORD"
+blunder = 4
  
 #enter the corresponding information from your Twitter application:
 CONSUMER_KEY = mykeys.CONSUMER_KEY
@@ -25,16 +26,21 @@ reply = re.compile('@[A-z]*')
 hashtag = re.compile('#[A-z]*')
 link = re.compile('https://[a-z, 0-9, ./]*')
  
-tweet = tweepy.Cursor(api.search, q=whatReplace).items(times)
- 
+tweet = tweepy.Cursor(api.search, q=whatReplace).items(times + blunder)
+
+counter = 0
 for tw in tweet:
-	if not (escapeword in tw.text):
+	print(tw.text.lower())
+	print(escapeword)
+	if not (escapeword in tw.text.lower()):
 	    txt=''
 	    tweetText = toReplace.sub(replaceWith, tw.text)
 	    for word in tweetText.split():
 	        exclusions = (reply.match(word) or word=='RT' or hashtag.match(word) or link.match(word))
 	        if not exclusions:
 	            txt+=(word+' ')
-	 
 	    api.update_status(txt.lower()) 
 	    time.sleep(timer)
+	    counter += 1
+	    if counter == times:
+	    	break
